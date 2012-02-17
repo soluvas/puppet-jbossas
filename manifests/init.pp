84602748c55f0c1db4d7082314fc9c6ff6698e7c
Hendy Irawan <hendy@soluvas.com>
2012-02-18 02:23:11 +0700

    Virtual Server support.

--- manifests/init.pp
+++ manifests/init.pp
@@ -134,4 +134,41 @@ class jbossas ($version = '7.1.0.Final',
 		require => Class['jbossas::install'],
 		notify => Service['jboss-as']
 	}
+	
+	define virtual_server($default_web_module = '',
+		$aliases = [],
+		$ensure = 'present'
+	) {
+		case $ensure {
+			'present': {
+#				notice "JBoss Virtual Server $name: default_web_module=$default_web_module"
+				if $default_web_module {
+					$cli_args = inline_template('<% require "json" %>default-web-module=<%= default_web_module %>,alias=<%= aliases.to_json.gsub("\"", "\\\"") %>')
+				} else {
+					$cli_args = inline_template("<% require 'json' %>alias=<%= aliases.to_json %>")
+				}
+				notice "$jbossas::dir/bin/jboss-cli.sh -c --command='/subsystem=web/virtual-server=$name:add\($cli_args\)'"
+				exec { "add jboss virtual-server $name":
+					command => "$jbossas::dir/bin/jboss-cli.sh -c --command=/subsystem=web/virtual-server=$name:add\\($cli_args\\)",
+					user => 'jbossas', group => 'jbossas',
+					logoutput => true,
+					unless => "/bin/sh $jbossas::dir/bin/jboss-cli.sh -c /subsystem=web/virtual-server=$name:read-resource | grep success",
+					require => Class['jbossas'],
+					notify => Service['jboss-as'],
+					provider => 'posix'
+				}
+			}
+			'absent': {
+				exec { "remove jboss virtual-server $name":
+					command => "$jbossas::dir/bin/jboss-cli.sh -c '/subsystem=web/virtual-server=$name:remove()'",
+					user => 'jbossas', group => 'jbossas',
+					logoutput => true,
+					onlyif => "/bin/sh $jbossas::dir/bin/jboss-cli.sh -c /subsystem=web/virtual-server=$name:read-resource | grep success",
+					require => Class['jbossas'],
+					notify => Service['jboss-as']
+				}
+			}
+		}
+	}
+
 }
