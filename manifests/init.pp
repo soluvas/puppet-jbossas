7832002b810b04a7a43856f02dcb4b10314e900e
Hendy Irawan <hendy@soluvas.com>
2012-02-17 13:52:16 +0700

    init.d script

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,10 +3,15 @@
 # This module manages JBoss Application Server 7.x
 #
 # Parameters:
+# * @version@ = '7.1.0.Final'
+# * @mirror_url@ = 'http://download.jboss.org/jbossas/7.1/jboss-as-7.1.0.Final/'
+# * @http_port@ = 8080
+# * @https_port@ = 8443
 #
 # Actions:
 #
 # Requires:
+# * package curl
 #
 # Sample Usage:
 #
@@ -18,14 +23,15 @@ class jbossas ($version = '7.1.0.Final',
 	$http_port = 8080,
 	$https_port = 8443)
 {
+	$dir = "/usr/share/jboss-as"
+	
 	class install {
 		$mirror_url_version = "${jbossas::mirror_url}jboss-as-${jbossas::version}.tar.gz"
 		$dist_dir = '/home/jbossas/tmp'
 		$dist_file = "${dist_dir}/jboss-as-${jbossas::version}.tar.gz"
-		$jboss_home = "/home/jbossas/jboss-as"
 
 		notice "Download URL: $mirror_url_version"
-		notice "JBoss AS directory: $jboss_home"
+		notice "JBoss AS directory: $jbossas::dir"
 	
 		# Create group, user, and home folder
 		group { jbossas:
@@ -58,34 +64,68 @@ class jbossas ($version = '7.1.0.Final',
 			owner => 'jbossas', group => 'jbossas',
 			mode => 0775,
 		}
-		exec { "/bin/tar -xz -f '$dist_file'":
+		exec { 'extract jboss':
+			command => "/bin/tar -xz -f '$dist_file'",
 			creates => "/home/jbossas/jboss-as-${jbossas::version}",
 			cwd => '/home/jbossas',
 			user => 'jbossas', group => 'jbossas',
 			logoutput => true,
-			unless => "/usr/bin/test -d '$jboss_home'"
+			unless => "/usr/bin/test -d '$jbossas::dir'"
 		}
-		exec { "/bin/mv -v 'jboss-as-${jbossas::version}' jboss-as":
-			creates => $jboss_home,
-			cwd => '/home/jbossas',
-			user => 'jbossas', group => 'jbossas',
+		exec { 'rename jboss_home':
+			command => "/bin/mv -v '/home/jbossas/jboss-as-${jbossas::version}' '${jbossas::dir}'",
+			creates => $jbossas::dir,
 			logoutput => true,
+			require => Exec['extract jboss']
+		}
+		file { "$jbossas::dir":
+			ensure => directory,
+			owner => 'jbossas', group => 'jbossas',
+			require => Exec['rename jboss_home']
+		}
+		
+	}
+
+	# init.d configuration for Ubuntu
+	class initd {
+		file { '/etc/jboss-as':
+			ensure => directory
+		}
+		file { '/etc/jboss-as/jboss-as.conf':
+			source => 'puppet:///modules/jbossas/init.d/jboss-as.conf',
+			mode => 0644,
+			require => File['/etc/jboss-as']
+		}
+		file { '/var/run/jboss-as':
+			ensure => directory,
+			owner => 'jbossas', group => 'jbossas',
+			mode => 0775
+		}
+		file { '/etc/init.d/jboss-as':
+			source => 'puppet:///modules/jbossas/init.d/jboss-as-standalone.sh',
+			mode => 0755
+		}
+		service { 'jboss-as':
+			enable => true,
+			require => File['/etc/init.d/jboss-as']
 		}
 	}
+	Class['install'] -> Class['initd']		
 	
 	include install
+	include initd
 	
 	# Set ports
 	notice "HTTP Port: $http_port - HTTPS Port: $https_port"
-	exec { "/bin/sed -i -e 's/socket-binding name=\"http\" port=\"[0-9]\\+\"/socket-binding name=\"http\" port=\"${http_port}\"/' jboss-as/standalone/configuration/standalone.xml":
+	exec { "/bin/sed -i -e 's/socket-binding name=\"http\" port=\"[0-9]\\+\"/socket-binding name=\"http\" port=\"${http_port}\"/' standalone/configuration/standalone.xml":
 		user => 'jbossas',
-		cwd => '/home/jbossas',
+		cwd => $dir,
 		logoutput => true,
 		require => Class['jbossas::install']
 	}
-	exec { "/bin/sed -i -e 's/socket-binding name=\"https\" port=\"[0-9]\\+\"/socket-binding name=\"https\" port=\"${https_port}\"/' jboss-as/standalone/configuration/standalone.xml":
+	exec { "/bin/sed -i -e 's/socket-binding name=\"https\" port=\"[0-9]\\+\"/socket-binding name=\"https\" port=\"${https_port}\"/' standalone/configuration/standalone.xml":
 		user => 'jbossas',
-		cwd => '/home/jbossas',
+		cwd => $dir,
 		logoutput => true,
 		require => Class['jbossas::install']
 	}
